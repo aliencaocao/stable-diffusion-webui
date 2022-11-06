3c970f0fcba0c7d4a083b8988eae3c5e89d8b829
Billy Cao <aliencaocao@gmail.com>
2022-11-06 19:50:07 +0800

    Enable insecure extensions access

--- webui-user.bat
+++ webui-user.bat
@@ -4,6 +4,6 @@ set PYTHON=
 set GIT=
 set VENV_DIR=-
 set SD_TEMP=
-set COMMANDLINE_ARGS=--force-enable-xformers --listen --deepdanbooru --api --disable-safe-unpickle --opt-channelslast
+set COMMANDLINE_ARGS=--force-enable-xformers --listen --deepdanbooru --api --disable-safe-unpickle --opt-channelslast --enable-insecure-extension-access
 
 call webui.bat

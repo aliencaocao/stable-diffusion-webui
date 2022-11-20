173fa4e7d0079de8b9c49f0e3e62d5acfe58b841
Billy Cao <aliencaocao@gmail.com>
2022-11-20 22:24:58 +0800

    Update webui bat

--- webui-user.bat
+++ webui-user.bat
@@ -4,6 +4,6 @@ set PYTHON=
 set GIT=
 set VENV_DIR=-
 set SD_TEMP=
-set COMMANDLINE_ARGS=--force-enable-xformers --listen --api --disable-safe-unpickle --opt-channelslast --enable-insecure-extension-access --gradio-debug --gradio-img2img-tool --theme dark
+set COMMANDLINE_ARGS=--force-enable-xformers --listen --api --disable-safe-unpickle --opt-channelslast --enable-insecure-extension-access --theme dark
 
 call webui.bat

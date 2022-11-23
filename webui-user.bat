30208b15c7fc4870861408f79eb66b5792690d52
Billy Cao <aliencaocao@gmail.com>
2022-11-23 17:37:09 +0800

    Swap stable diffusion repo with my own to support resolutions that are not multiples of 64

--- webui-user.bat
+++ webui-user.bat
@@ -4,6 +4,7 @@ set PYTHON=
 set GIT=
 set VENV_DIR=-
 set SD_TEMP=
+set ACCELERATE=True
 set COMMANDLINE_ARGS=--force-enable-xformers --listen --api --disable-safe-unpickle --opt-channelslast --enable-insecure-extension-access --theme dark
 
 call webui.bat

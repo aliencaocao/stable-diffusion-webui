e2b561a85fab3f36ab440f404a0597e42863c401
Billy Cao <aliencaocao@gmail.com>
2023-01-30 18:10:58 +0800

    change launch options

--- webui-user.bat
+++ webui-user.bat
@@ -5,6 +5,6 @@ set GIT=
 set VENV_DIR=-
 set SD_TEMP=
 set SAFETENSORS_FAST_GPU=1
-set COMMANDLINE_ARGS=--force-enable-xformers --listen --api --disable-safe-unpickle --opt-channelslast --enable-insecure-extension-access --theme dark
+set COMMANDLINE_ARGS=--xformers --listen --api --disable-safe-unpickle --opt-channelslast --enable-insecure-extension-access --theme dark --skip-python-version-check
 
 call webui.bat

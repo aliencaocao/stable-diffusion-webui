066a43845dfeeeca912111b0da1de05d3ec3ad37
Billy Cao <aliencaocao@gmail.com>
2023-08-01 21:35:42 +0800

    Update cmd args Fix compat with new torch lightening versions

--- webui-user.bat
+++ webui-user.bat
@@ -4,7 +4,7 @@ set PYTHON=
 set GIT=
 set VENV_DIR=-
 set SD_TEMP=
-set SAFETENSORS_FAST_GPU=1
-set COMMANDLINE_ARGS=--opt-sdp-attention --listen --api --disable-safe-unpickle --opt-channelslast --enable-insecure-extension-access --theme dark --skip-python-version-check --skip-prepare-environment
+set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.9,max_split_size_mb:512
+set COMMANDLINE_ARGS=--opt-sdp-attention --listen --api --disable-safe-unpickle --opt-channelslast --enable-insecure-extension-access --theme dark --skip-python-version-check --skip-install --update-check --update-all-extensions --skip-prepare-environment --opt-split-attention --ckpt-dir "D:\Stable Diffusion checkpoints"
 
 call webui.bat
